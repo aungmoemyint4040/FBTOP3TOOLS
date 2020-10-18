d40e10003f2fccfecac51f1a664c3563af3020c8
aungmoemyint404 <73078950+aungmoemyint404@users.noreply.github.com>
2020-10-18 15:21:32 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+apt install ruby -y
+gem install lolcat
+apt install python -y
+apt install python2 -y
+apt install git -y
+apt install openssh -y
+apt install wget -y
+apt install php -y
+pip2 install requests
+pip2 install mechanize
+clear
+echo '            Seccssful Install ' | lolcat -a -d 30
+sleep 2
+clear
+echo '         TOOL USARNAME =fatabotol ' | lolcat -a -d 10
+echo '         TOOL PASSWORD =fatabotol ' | lolcat -a -d 10
+
+
